a0e99b3d81bf21ab056c8f12c0df472c38404c9a
maivanhung33 <45779896+maivanhung33@users.noreply.github.com>
2019-11-30 14:26:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
 from common.application import Application
 
-# app = Application.get_instance().get_app()
-
-if __name__ == '__main__':
-    Application \
-        .get_instance() \
-        .run()
+app = Application.get_instance().get_app()
